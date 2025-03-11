6e10a24b55400a659543456f7173e5284a1740c0
Macsaroo <sajjad.sarookhani@gmail.com>
2025-03-11 19:55:11 +0330

    some changes to customer classes and write main of the program

--- src/Main.java
+++ src/Main.java
@@ -12,21 +12,25 @@ public class Main {
         mahmood.displayCustomerInfo();
 
         PayPalPayment ali_Paypal = new PayPalPayment("Ali@gmail.com");
-        ali_Paypal.pay(12000);
+        ali.makePayment(ali_Paypal,12000);
         CreditCardPayment ali_Credit = new CreditCardPayment("6219-8619" , "Ali Alizade Shemrooni");
-        ali_Credit.pay(20000);
+        ali.makePayment(ali_Credit,20000);
+
 
         PayPalPayment reza_Paypal = new PayPalPayment("Reza@gmail.com");
-        reza_Paypal.pay(50000);
+        reza.makePayment(reza_Paypal,50000);
         CreditCardPayment reza_Credit = new CreditCardPayment("6210-8010" , "Reza Rezazade");
-        reza_Credit.pay(25000);
+        reza.makePayment(reza_Credit,25000);
 
         BitcoinPayment mahmood_Bitcoin = new BitcoinPayment("1234569");
-        mahmood_Bitcoin.pay(50000);
+        mahmood.makePayment(mahmood_Bitcoin,50000);
         PayPalPayment mahmood_Credit = new PayPalPayment("mahmood@gmail.com");
-        mahmood_Credit.pay(30000);
+        mahmood.makePayment(mahmood_Credit,30000);
 
-        
+
+        ali.showPaymentHistory();
+        reza.showPaymentHistory();
+        mahmood.showPaymentHistory();
 
     }
 }
--- src/payment/Customer.java
+++ src/payment/Customer.java
@@ -14,12 +14,14 @@ public abstract class Customer {
         paymentStrategy.pay(amount);
         String data = paymentStrategy.getPaymentDetails();
         String[] split = data.split(",");
-        paymentHistory.add("amount:"+ amount + "type:" + split[0]);
+        paymentHistory.add("amount:"+ amount + " type:" + split[0]);
     }
 
     public void showPaymentHistory() {
+        System.out.println("Showing payment history for " + name);
         for (String s : paymentHistory) {
             System.out.println(s);
         }
+        System.out.println();
     }
 }
